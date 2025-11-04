096f818679e598b2ed07ab8eaf6d904bf6e8f2f2
Vladimir <antonovvaldemar5@gmail.com>
2025-11-04 17:31:16 +0300

    fix: изменил порядок сортировки по умолчанию на от новых к старым

--- src/processing.py
+++ src/processing.py
@@ -4,13 +4,9 @@ from typing import List, Dict, Any
 def filter_by_state(
         operations: List[Dict[str, Any]], state: str = "EXECUTED"
 ) -> List[Dict[str, Any]]:
-    """
-    Фильтрует банковские операции по статусу.
+    """Фильтрует банковские операции по статусу."""
 
-    Берет список операций и оставляет только те,
-    у которых статус совпадает с переданным значением.
-    Если не указать статус, будет искать 'EXECUTED'.
-    """
+
     result = []
     for operation in operations:
         if operation.get("state") == state:
@@ -19,14 +15,10 @@ def filter_by_state(
 
 
 def sort_by_date(
-        operations: List[Dict[str, Any]], reverse: bool = False
+        operations: List[Dict[str, Any]], reverse: bool = True
 ) -> List[Dict[str, Any]]:
-    """
-    Сортирует банковские операции по дате.
+    """ Сортирует банковские операции по дате. """
 
-    Упорядочивает операции от старых к новым.
-    Если передать reverse=True, будет от новых к старым.
-    """
     sorted_operations = sorted(
         operations, key=lambda x: x.get("date", ""), reverse=reverse
     )
